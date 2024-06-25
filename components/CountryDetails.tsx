499b5a14f4691d3249c31010c438fdb0822198b5
aliraza <chaudaryaliraza0@gmail.com>
2024-06-25 12:41:31 +0500

    add empty details and search pages

---
+++ components/CountryDetails.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CountryDetails = () => {
+  return (
+    <View>
+      <Text>CountryDetails</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default CountryDetails
