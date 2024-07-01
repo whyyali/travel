080e1ff45977db4c0207e459a2b42096f1e7728c
aliraza <chaudaryaliraza0@gmail.com>
2024-07-01 19:34:23 +0500

    country detail

--- app/CountryDetail.tsx
+++ app/CountryDetail.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet, SafeAreaView, ScrollView } from 'react-native'
 import { RouteProp, useRoute } from '@react-navigation/native'
+import { ImageBox } from '@/components'
+import { images } from '@/images'
+import { COLORS, SIZES } from '@/constants/Colors'
 
 type CountryDetailRouteProp = RouteProp<{ CountryDetail: { item: { _id: string, country: string, image: any } } }, 'CountryDetail'>
 
@@ -9,11 +12,13 @@ const CountryDetail: React.FC = () => {
     const { item } = route.params
 
     return (
-        <View style={styles.container}>
-            <Image source={item.image} style={styles.image} />
-            <Text style={styles.countryName}>{item.country}</Text>
-            {/* Add more details as needed */}
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{ position: "relative" }}>
+                    <ImageBox source={images[item.country.toLowerCase()]} width={SIZES.width - 10} height={250} radius={30} />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
@@ -27,11 +32,6 @@ const styles = StyleSheet.create({
         width: 150,
         height: 150,
         borderRadius: 12,
-    },
-    countryName: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 10,
     },
 })
 
